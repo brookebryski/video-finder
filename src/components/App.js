a22ad4a5fa8824ca369e11cfcd6361faae5ff334
github username <github email address>
2021-04-15 11:48:36 -0500

    edits

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import { ReactComponent } from '*.svg';
 import React from 'react';
-import SearchBar from './SearchBar.js';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render() {
