b758e67c58c1a23a7a8f1cb54f2875f6543b260e
github username <github email address>
2021-04-19 10:24:04 -0500

    implemented youtube api

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyAX-ox0LwcKJIGOA3ParkehC1PZYUsJ56g';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
